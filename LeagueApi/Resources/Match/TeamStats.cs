c8c830a045d11148a2bb4cdf33d325990099d046
MrJackWilson <jack@brickwall.uk.com>
2018-08-04 17:46:29 +0100

    Added team stats

---
+++ LeagueApi/Resources/Match/TeamStats.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeagueApi.Resources.Match
+{
+    public class TeamStats
+    {
+        [JsonProperty("firstDragon")]
+        public bool FirstDragon { get; set; }
+
+        [JsonProperty("firstInhibitor")]
+        public bool FirstInhibitor { get; set; }
+    }
+}
